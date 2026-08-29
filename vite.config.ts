54a581838b928bfbe479ce734a81a32b073a1387
zeyhermutter <playsony@web.de>
2026-08-29 22:38:47 +0200

    feat: add Cloudflare Vite configuration

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
+import { reactRouter } from "@react-router/dev/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    reactRouter(),
+  ],
+});
