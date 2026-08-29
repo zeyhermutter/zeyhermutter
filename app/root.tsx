d5da78a13ae470944e9c8454e5263b15ded9fa2a
zeyhermutter <playsony@web.de>
2026-08-29 23:04:48 +0200

    style: load CRM editor styles

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
 } from "react-router";
 import type { Route } from "./+types/root";
 import "./styles.css";
+import "./crm.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
