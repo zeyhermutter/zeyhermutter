13c8388b63bd3093a3b4843020faae59ea7485f8
zeyhermutter <playsony@web.de>
2026-09-01 18:05:23 +0200

    Beta: join exact uploaded logo tail

--- app/brand-logo-data/index.ts
+++ app/brand-logo-data/index.ts
@@ -1,4 +1,5 @@
 import part1 from "./part1";
+import part1Tail from "./part1-tail";
 import part2 from "./part2";
 import part3 from "./part3";
 import part4 from "./part4";
@@ -7,4 +8,4 @@ import part6 from "./part6";
 import part7 from "./part7";
 import part8 from "./part8";
 
-export const brandLogoDataUri = `data:image/png;base64,${part1}${part2}${part3}${part4}${part5}${part6}${part7}${part8}`;
+export const brandLogoDataUri = `data:image/png;base64,${part1}${part1Tail}${part2}${part3}${part4}${part5}${part6}${part7}${part8}`;
