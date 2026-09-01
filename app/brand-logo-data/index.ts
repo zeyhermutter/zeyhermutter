3586c07cf16a940ce3078e9b947fdc568c971ae0
zeyhermutter <playsony@web.de>
2026-09-01 17:45:20 +0200

    Beta: assemble uploaded brand logo asset

---
+++ app/brand-logo-data/index.ts
@@ -0,0 +1,10 @@
+import part1 from "./part1";
+import part2 from "./part2";
+import part3 from "./part3";
+import part4 from "./part4";
+import part5 from "./part5";
+import part6 from "./part6";
+import part7 from "./part7";
+import part8 from "./part8";
+
+export const brandLogoDataUri = `data:image/png;base64,${part1}${part2}${part3}${part4}${part5}${part6}${part7}${part8}`;
